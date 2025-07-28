e0365120359eb6cf78cdc340e9c911ee8d587fcb
cuentapro11 <beboagua9@gmail.com>
2025-07-28 14:06:53 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/rosmery-celebration-countdown">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
